adc6097e6bec0a381e8ba6e1fe2de92a9336510c
surfacez <sergiobatistajr94@gmail.com>
2024-01-15 10:46:57 -0300

    add more validation

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,21 +1,29 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @IsString()
   @Field()
   firstName: string;
 
+  @IsString()
   @Field()
   lastName: string;
 
+  @IsString()
   @IsEmail()
   @Field()
   email: string;
 
+  @Length(8, 64)
+  @IsString({
+    message: 'Password must be a string',
+  })
   @Field()
   password: string;
 
+  @IsString()
   @Field()
   role: string;
 }
